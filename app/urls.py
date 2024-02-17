35a91b55cbc382b22c5c0e90ea2d98c16263bba6
prasanna89859 <mprasanna015@gmail.com>
2024-02-17 10:47:05 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from app.views import *
+app_name='prasanna'
+
+urlpatterns =[
+    path('prasanna/',prasanna,name='prasanna'),
+
+]
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def respones(request):
+    return render(request,'respones.html')
